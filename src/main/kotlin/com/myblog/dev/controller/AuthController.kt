184187e5470db090aceac4e22eeb7d180c8feb6b
kannna5296 <kannnahacks5296@gmail.com>
2024-08-12 16:35:40 +0900

    EndPoint追加

--- src/main/kotlin/com/myblog/dev/controller/AuthController.kt
+++ src/main/kotlin/com/myblog/dev/controller/AuthController.kt
@@ -1,4 +1,23 @@
 package com.myblog.dev.controller
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/auth")
 class AuthController {
+
+    @PostMapping("/login")
+    fun login(): ResponseEntity<String> {
+        println("login")
+        return ResponseEntity.ok("login requested")
+    }
+
+    @PostMapping("/register")
+    fun register(): ResponseEntity<String> {
+        println("register")
+        return ResponseEntity.ok("register requested")
+    }
 }
