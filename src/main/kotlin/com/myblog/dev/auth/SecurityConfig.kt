92217d267ae457e85d1e124951b9c1b4b0dde07e
kannna5296 <kannnahacks5296@gmail.com>
2024-10-03 09:03:40 +0900

    コメント追加

--- src/main/kotlin/com/myblog/dev/auth/SecurityConfig.kt
+++ src/main/kotlin/com/myblog/dev/auth/SecurityConfig.kt
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +29,10 @@ class SecurityConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .csrf { csrf -> csrf.ignoringRequestMatchers(AuthLessPath.AUTHLESS_PATH_MATCHERS) }
+            .csrf { csrf ->
+                csrf.ignoringRequestMatchers(AuthLessPath.AUTHLESS_PATH_MATCHERS)
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            }
             .cors { cors -> cors.configurationSource(corsConfig) }
             .authorizeHttpRequests { auth ->
                 auth
