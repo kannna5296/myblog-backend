e282d9a418da09ffa30b228de27b2bbf014c6ef9
kannna5296 <kannnahacks5296@gmail.com>
2024-10-15 09:20:49 +0900

    Swagger自動生成機能ついか

--- src/main/kotlin/com/myblog/dev/auth/AuthLessPath.kt
+++ src/main/kotlin/com/myblog/dev/auth/AuthLessPath.kt
@@ -6,7 +6,7 @@ import org.springframework.security.web.util.matcher.OrRequestMatcher
 class AuthLessPath {
 
     companion object {
-        val AUTHLESS_PATH = setOf("/auth/**", "/error/**")
+        val AUTHLESS_PATH = setOf("/auth/**", "/error/**", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml")
         val AUTHLESS_PATH_MATCHERS = OrRequestMatcher(AUTHLESS_PATH.map { AntPathRequestMatcher(it) })
     }
 }
