5692a460168c17069e75327961c8038e2fce25e1
kannna5296 <kannnahacks5296@gmail.com>
2024-10-20 20:21:21 +0900

    要らんやつ削除

--- src/main/kotlin/com/myblog/dev/auth/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/myblog/dev/auth/JwtAuthenticationFilter.kt
@@ -4,8 +4,6 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
@@ -51,11 +49,6 @@ class JwtAuthenticationFilter(
                 println("トークンが有効ではない")
                 throw UnAuthorizedException()
             }
-
-            val authentication = UsernamePasswordAuthenticationToken(
-                userDetails, null, userDetails.authorities
-            )
-            SecurityContextHolder.getContext().authentication = authentication
 
             filterChain.doFilter(request, response)
         } catch (e: UnAuthorizedException) {
